b320eb937349a99f5e7ed1451fa7811559dab485
Chris Turner <chris@templemore.co.uk>
2013-05-09 20:15:08 +0100

    Dojo development of the game module

--- connect4/server/scala-template/src/main/scala/GameModule.scala
+++ connect4/server/scala-template/src/main/scala/GameModule.scala
@@ -1,5 +1,27 @@
 package com.equalexperts.connect4
 
+import scala.collection.immutable.Stack
+import scala.util.Try
+
+
 trait GameModule {
   var credits = "Connect4 Server. (c)2013 Equal Experts Limited. All Rights Reserved."
+
+  sealed trait Piece
+  case object RedPiece extends Piece
+  case object YellowPiece extends Piece
+
+  type Grid = Seq[Stack[Piece]]
+
+  def createGame: Grid = {
+    (1 to 7) map (_ => Stack.empty)
+  }
+
+  def addPiece(grid: Grid, piece: Piece, column: Int) : Try[Grid] =  Try {
+    val columnIndex = column-1
+    val targetColumn = grid(columnIndex)
+    grid.updated(columnIndex, targetColumn push piece)
+  }
+
+
 }
--- connect4/server/scala-template/src/test/scala/GameModuleSpec.scala
+++ connect4/server/scala-template/src/test/scala/GameModuleSpec.scala
@@ -5,6 +5,10 @@ import org.specs2.ScalaCheck
 import org.specs2.matcher.MatchResult
 import org.scalacheck._
 
+import scala.collection.immutable.Stack
+import scala.util.Try
+import scala.util.Success
+
 
 class GameModuleSpec extends Specification with ScalaCheck with GameModule { def is =
 
@@ -12,12 +16,39 @@ class GameModuleSpec extends Specification with ScalaCheck with GameModule { def
                                                                                       endp^
   "Retrieving the credits should"                                                     ^
     "return the credit string"                                                        ! checkCredits^
+                                                                                      endp^
+  "Creating a new connect 4 game instance should"                                     ^
+    "return an empty game board"                                                      ! newGame^
+                                                                                      endp^
+  "A piece can be added to the board should"                                          ^
+    "returns an updated board"                                                        ! firstPiece^
+    "returns an error if move is invalid"                                             ! addInvalidPiece^
+    "can be added after one is already present"                                       ! secondPiece^
                                                                                       end
+
 
   import Prop.forAll
   import Arbitrary.arbitrary
 
   def checkCredits = {
     credits must_== "Connect4 Server. (c)2013 Equal Experts Limited. All Rights Reserved."
+  }
+
+  def newGame = 
+    createGame must_== Seq(Stack(), Stack(), Stack(), Stack(), Stack(), Stack(), Stack())
+
+  def firstPiece = 
+     addPiece(createGame, RedPiece, 3) must_== Success(Seq(Stack(), Stack(), Stack(RedPiece), Stack(), Stack(), Stack(), Stack()))
+
+  def addInvalidPiece =
+     addPiece(createGame, RedPiece, 1024).isFailure must beTrue 
+
+  def secondPiece = {
+    val result = for{
+      first <- addPiece(createGame, RedPiece, 3)
+      second <- addPiece(first, YellowPiece, 3) 
+    } yield second
+    
+    result must_== Success(Seq(Stack(), Stack(), Stack(YellowPiece, RedPiece), Stack(), Stack(), Stack(), Stack()))
   }
 }
